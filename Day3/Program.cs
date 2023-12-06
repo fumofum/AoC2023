ba21f5d12d465b3cfec20453b2c159663e8d2b76
fumofum <>
2023-12-06 18:38:45 +0300

    Day 3 (not done yet)

---
+++ Day3/Program.cs
@@ -0,0 +1,35 @@
+﻿// ******************************************
+// ******************************************
+// https://adventofcode.com/2023/day/3
+// ******************************************
+// ******************************************
+
+namespace Day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = "input.txt";
+
+            var linesFromFile = GetLinesFromFile(inputFilePath);
+
+            foreach(var line in linesFromFile) 
+            {
+
+            }
+        }
+
+        static IEnumerable<string> GetLinesFromFile(string filePath)
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                var currentLine = string.Empty;
+                while ((currentLine = sr.ReadLine()?.ToLower()) != null)
+                {
+                    yield return currentLine;
+                }
+            }
+        }
+    }
+}
