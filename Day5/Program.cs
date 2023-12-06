091acf7c34018f2a5771998c60ab1589f1359df9
fumofum <>
2023-12-06 18:38:08 +0300

    Day 5 (not optimized), day 6

---
+++ Day5/Program.cs
@@ -0,0 +1,90 @@
+﻿// ******************************************
+// ******************************************
+// https://adventofcode.com/2023/day/5
+// ******************************************
+// ******************************************
+
+namespace Day5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputFilePath = "input.txt";
+            var allBlocks = File.ReadAllText(inputFilePath).Split("\n\n", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            var seeds = allBlocks.First()
+                                 .Split(':')[1]
+                                 .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                 .Select(ulong.Parse)
+                                 .ToArray();
+
+            var parsedMapBlocks = allBlocks.Skip(1)
+                                           .Select(ParseMapBlock)
+                                           .ToList();
+
+            ulong locationNumber = 0;
+
+            for(int i = 0; i < seeds.Length; i += 2)
+            {
+                ulong endOfRange = seeds[i] + seeds[i + 1];
+
+                for (ulong j = seeds[i]; j < endOfRange; ++j)
+                {
+                    var currentValue = j;
+                    foreach (var mapBlock in parsedMapBlocks)
+                    {
+                        currentValue = GetNextValue(currentValue, mapBlock);
+                    }
+
+                    if (locationNumber == 0 || locationNumber > currentValue)
+                    {
+                        locationNumber = currentValue;
+                    }
+                }
+            }
+
+            Console.WriteLine($"Lowest location number: {locationNumber}");
+        }
+
+        //static List<(ulong start, ulong end)> SplitRange((ulong start, ulong end) range, List<(ulong distination, ulong source, ulong length)> block)
+        //{
+        //    var resultRanges = new List<(ulong start, ulong end)>();
+
+        //    if(block.FirstOrDefault(x => x.source >= range.start && range.end <= x.source + x.length) != default)
+        //    {
+        //        return Enumerable.Repeat(range, 1).ToList();
+        //    }
+
+
+        //}
+
+        static ulong GetNextValue(ulong currentValue, List<(ulong distination, ulong source, ulong length)> block)
+        {
+            (ulong distination, ulong source, ulong length) line = block.Where(x => currentValue >= x.source && currentValue <= x.source + x.length - 1)
+                                                                        .FirstOrDefault();
+
+            if(line == default)
+                return currentValue;
+
+            return line.distination + (currentValue - line.source); 
+        }
+
+        static List<(ulong, ulong, ulong)> ParseMapBlock(string blockData)
+        {
+            var lines = blockData.Split('\n', StringSplitOptions.RemoveEmptyEntries).Skip(1).ToList();
+
+            var parsedBlock = new List<(ulong, ulong, ulong)>(lines.Count);
+            foreach(var line in lines)
+            {
+                var splittedLine = line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                       .Select(ulong.Parse)
+                                       .ToList();
+                
+                parsedBlock.Add((splittedLine[0], splittedLine[1], splittedLine[2]));
+            }
+
+            return parsedBlock.OrderBy(x => x.Item2).ToList();
+        }
+    }
+}
---
+++ Day6/Program.cs
@@ -0,0 +1,46 @@
+﻿// ******************************************
+// ******************************************
+// https://adventofcode.com/2023/day/6
+// ******************************************
+// ******************************************
+
+namespace Day6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputData = File.ReadAllLines("input.txt");
+
+            Console.WriteLine($"(Part 1) Winning ways mul:{Part1(inputData)}");
+            Console.WriteLine($"(Part 2) Winning ways:{Part2(inputData)}");
+        }
+
+        static int Part1(string[] inputData)
+        {
+            var time = inputData[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(uint.Parse).ToArray();
+            var distance = inputData[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1).Select(uint.Parse).ToArray();
+
+            return Enumerable.Range(0, time.Length)
+                             .Select(i => GetWinningWays(time[i], distance[i]).Count())
+                             .Aggregate((x, y) => x * y);
+        }
+
+        static int Part2(string[] inputData)
+        {
+            var time = ulong.Parse(string.Join("", inputData[0].Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1)));
+            var distance = ulong.Parse(string.Join("", inputData[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).Skip(1)));
+
+            return GetWinningWays(time, distance).Count();
+        }
+
+        static IEnumerable<uint> GetWinningWays(ulong timeLimit, ulong recordDistance)
+        {
+            for(uint holdingButtonTime = 1; holdingButtonTime < timeLimit - 1; ++holdingButtonTime)
+            {
+                if(holdingButtonTime * (timeLimit - holdingButtonTime) > recordDistance)
+                    yield return holdingButtonTime;
+            }
+        }
+    }
+}
